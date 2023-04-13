781dc99d1c088d903144f52493ace3aa031f2d92
Daniel P <46326833+moshidev@users.noreply.github.com>
2023-04-13 16:09:26 +0200

    mejora estructura de datos

--- P3/software/ejercicio_2/ejercicio_2.c
+++ P3/software/ejercicio_2/ejercicio_2.c
@@ -19,51 +19,38 @@ struct pthread_calc_max;
 
 struct pthread_calc_max_context {
     size_t thread_id;
-    size_t thread_count;
-    const double* vector;
-    size_t vector_len;
-    size_t* solution_offset;
-    pthread_mutex_t* solution_mutex;
+    struct pthread_calc_max* parent;
 };
 
 struct pthread_calc_max {
     const double* vector;
-    const size_t vector_len;
+    size_t vector_len;
     pthread_t* thread;
     struct pthread_calc_max_context* context;
     size_t thread_count;
-    size_t* solution_offset;
-    pthread_mutex_t* solution_mutex;
+    size_t solution_offset;
+    pthread_mutex_t solution_mutex;
 };
 
-static struct pthread_calc_max make_pthread_calc_max(size_t thread_count, const double* vector, size_t vector_len) {
-    struct pthread_calc_max ret = {
-        .vector = vector,
-        .vector_len = vector_len,
-        .thread = calloc(thread_count, sizeof(pthread_t)),
-        .context = calloc(thread_count, sizeof(struct pthread_calc_max_context)),
-        .solution_offset = calloc(1, sizeof(double)),
-        .solution_mutex = calloc(1, sizeof(pthread_mutex_t)),
-        .thread_count = thread_count
-    };
-    pthread_mutex_init(ret.solution_mutex, NULL_PTR);
+static struct pthread_calc_max* make_pthread_calc_max(size_t thread_count, const double* vector, size_t vector_len) {
+    struct pthread_calc_max* ret = malloc(sizeof(struct pthread_calc_max) + thread_count*sizeof(pthread_t) + thread_count*sizeof(struct pthread_calc_max_context));
+    ret->vector = vector;
+    ret->vector_len = vector_len;
+    ret->thread = (pthread_t*) (((uint8_t*)ret) + sizeof(struct pthread_calc_max));
+    ret->context = (struct pthread_calc_max_context*) (((uint8_t*)ret) + sizeof(struct pthread_calc_max) + thread_count*sizeof(pthread_t));
+    ret->solution_offset = 0;
+    pthread_mutex_init(&ret->solution_mutex, NULL_PTR);
+    ret->thread_count = thread_count;
     for (size_t i = 0; i < thread_count; i++) {
-        ret.context[i].thread_id = i;
-        ret.context[i].thread_count = thread_count;
-        ret.context[i].vector = vector;
-        ret.context[i].vector_len = vector_len;
-        ret.context[i].solution_offset = ret.solution_offset;
-        ret.context[i].solution_mutex = ret.solution_mutex;
+        ret->context[i].thread_id = i;
+        ret->context[i].parent = ret;
     }
     return ret;
 }
 
 static void destroy_pthread_calc_max(struct pthread_calc_max* p) {
-    pthread_mutex_destroy(p->solution_mutex);
-    free(p->thread);
-    free(p->context);
-    free(p->solution_offset);
-    free(p->solution_mutex);
+    pthread_mutex_destroy(&p->solution_mutex);
+    free(p);
 }
 
 size_t calc_max_seq(const double* v, size_t vlen) {
@@ -80,41 +67,42 @@ size_t calc_max_seq(const double* v, size_t vlen) {
 
 void* pthread_calc_max_worker(void* pthread_user_data) {
     struct pthread_calc_max_context* c = (struct pthread_calc_max_context*)pthread_user_data;
+    struct pthread_calc_max* p = c->parent;
 
-    size_t len = c->vector_len / c->thread_count;
-    if (c->thread_id == c->thread_count-1) {
-        len += c->vector_len % c->thread_count;
+    size_t len = p->vector_len / p->thread_count;
+    if (c->thread_id == p->thread_count-1) {
+        len += p->vector_len % p->thread_count;
     }
 
     size_t offset = len * c->thread_id;
-    const double* v = c->vector + offset;
+    const double* v = p->vector + offset;
     size_t lsolution_offset = calc_max_seq(v, len) + offset;
-    pthread_mutex_lock(c->solution_mutex);
-    if (c->vector[lsolution_offset] > c->vector[*c->solution_offset]) {
-        *c->solution_offset = lsolution_offset;
+    pthread_mutex_lock(&p->solution_mutex);
+    if (p->vector[lsolution_offset] > p->vector[p->solution_offset]) {
+        p->solution_offset = lsolution_offset;
     }
-    pthread_mutex_unlock(c->solution_mutex);
+    pthread_mutex_unlock(&p->solution_mutex);
 
-    pthread_exit(NULL_PTR);
+    pthread_exit(c);
 }
 
 size_t calc_max_parallel(size_t thread_count, const double* v, size_t vlen) {
-    struct pthread_calc_max pexec = make_pthread_calc_max(thread_count, v, vlen);
+    struct pthread_calc_max* pexec = make_pthread_calc_max(thread_count, v, vlen);
     
     for (size_t i = 0; i < thread_count; i++) {
-        pthread_create(&pexec.thread[i], NULL, pthread_calc_max_worker, &pexec.context[i]);
+        pthread_create(&pexec->thread[i], NULL, pthread_calc_max_worker, &pexec->context[i]);
     }
 
     size_t max_i = 0;
     for (size_t i = 0; i < thread_count; i++) {
-        pthread_join(pexec.thread[i], NULL_PTR);
+        pthread_join(pexec->thread[i], NULL_PTR);
 
-        if (v[*pexec.solution_offset] > v[max_i]) {
-            max_i = *pexec.solution_offset;
+        if (v[pexec->solution_offset] > v[max_i]) {
+            max_i = pexec->solution_offset;
         }
     }
 
-    destroy_pthread_calc_max(&pexec);
+    destroy_pthread_calc_max(pexec);
     return max_i;
 }
 
