45641bff390a00b0834a3da0fcf89cdc81fffc21
Daniel P <46326833+moshidev@users.noreply.github.com>
2023-04-22 12:09:36 +0200

    calcula la cardinalidad de la intersección de dos conjuntos

--- P3/software/ejercicio_3/ejercicio_3.c
+++ P3/software/ejercicio_3/ejercicio_3.c
@@ -1,5 +1,5 @@
 #include "khash.h"
-#include <pthread.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,9 +8,9 @@
 const char txt_err_dist[] = "No puedo ejecutar el ejercicio si no me indicas la longitud de los conjuntos sobre los que calcular la distancia de Jaccard >:c\n";
 
 static double get_wall_time() {
-    struct timeval time;
-    gettimeofday(&time,NULL);
-    return (double)time.tv_sec + (double)time.tv_usec * .000001;
+	struct timeval time;
+	gettimeofday(&time,NULL);
+	return (double)time.tv_sec + (double)time.tv_usec * .000001;
 }
 
 static void assert_argc(int argc, char** argv) {
@@ -30,30 +30,58 @@ static void assert_set_len(size_t va_len, size_t vb_len, char* command, char* va
 	}
 }
 
+KHASH_SET_INIT_INT(i32)
+
 int main(int argc, char** argv) {
 	assert_argc(argc, argv);
 
 	size_t va_len = strtoul(argv[1], 0, 0);
 	size_t vb_len = strtoul(argv[2], 0, 0);
-	char smallest_v_char = va_len < vb_len ? 'a' : 'b';
-	size_t smallest_v_len = smallest_v_char == 'a' ? va_len : vb_len; 
+	size_t smallest_v_len = va_len < vb_len ? va_len : vb_len;
+	size_t biggest_v_len = va_len >= vb_len ? va_len : vb_len;
 
 	assert_set_len(va_len, vb_len, argv[0], argv[1], argv[2]);
 
 	printf("Reserva memoria...\n");
 	int32_t* va = calloc(va_len, sizeof(int32_t));
 	int32_t* vb = calloc(vb_len, sizeof(int32_t));
-	int32_t* smallest_v = smallest_v_char == 'a' ? va : vb;
 
 	printf("Inicializa vectores...\n");
-	for (size_t i = 0; i < smallest_v_len; i++) {
+	for (size_t i = 0; i < biggest_v_len; i++) {
 		if (i < va_len) {
 			va[i] = i%1000;
 		}
 		if (i < vb_len) {
-			vb[i] = i%1000;
+			vb[i] = i%39;
 		}
 	}
+
+	printf("Inicializa hashset...\n");
+	khash_t(i32)* hset_a = kh_init(i32);
+	for (size_t i = 0; i < va_len; i++) {
+		int retc;
+		kh_put(i32, hset_a, va[i], &retc);
+		if (retc < 0) {
+			fprintf(stderr, "o.o no puedo insertar la clave %d en el hashset. ¿Por qué?\n", va[i]);
+			fprintf(stderr, "Error fatal.\n");
+			exit(39);
+		}
+	}
+
+	printf("Calcula el # de la intersección de los dos conjuntos...\n");
+	khash_t(i32)* hset_b = kh_init(i32);
+	size_t intersect_count = 0;
+	for (size_t i = 0; i < vb_len; i++) {
+		int retc;
+		kh_put(i32, hset_b, vb[i], &retc);
+		if (retc == 1) {
+			khint_t it = kh_get(i32, hset_a, vb[i]);
+			bool is_missing = (it == kh_end(hset_a));
+			intersect_count += is_missing ? 0 : 1;
+		}
+	}
+
+	printf("Cardinalidad encontrada: %zu\n", intersect_count);
 
 	return 0;
 }
