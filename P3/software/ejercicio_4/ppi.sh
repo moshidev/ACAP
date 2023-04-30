0ac95765dcaaa4a1f43cba4b3f46ab530a865261
Daniel P <46326833+moshidev@users.noreply.github.com>
2023-05-01 00:07:10 +0200

    escribe memoria

--- P3/software/ejercicio_4/ppi.sh
+++ P3/software/ejercicio_4/ppi.sh
@@ -9,23 +9,23 @@ function wait_until_no_jobs() {
 	done 
 }
 
-run='sbatch -Aacap -p acap --exclusive --hint=nomultithread -N 1 -c 1 -n 6 --wrap'
-#run='bash -c'
+#run='sbatch -Aacap -p acap --exclusive --threads-per-core=1 --cpus-per-task=4 --ntasks 6 --wrap'
+run='bash -c'
 output_file=ppi.csv
-nprocs=6
-nhyperthreads=2
+nprocs=4
+ncores=4
 
 make
 
-wait_until_no_jobs
+#wait_until_no_jobs
 $run "mpirun -np 1 ./ppi_exe 1 1 | head -n 1 > $output_file"
 for i in `seq 1 1 $nprocs`
 do
-	for t in `seq 1 1 $(($nprocs*$nhyperthreads / $i))`
+	for t in `seq 1 1 $(($ncores - $i + 1))`
 	do
-		wait_until_no_jobs
+		#wait_until_no_jobs
 		$run "mpirun -np $i ./ppi_exe 2147483647 $t | tail -n 1 >> $output_file"
 	done
 done
 
-wait_until_no_jobs
+#wait_until_no_jobs
